2dde4b988ce3e46606e7383a59c9324fbed54ee8
Norbert Borek <norbertborek92@gmail.com>
2022-10-15 14:17:23 +0200

    done

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int b = Math.max(first, second);
+        System.out.println(b);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result = 0;
+        int temp = 0;
+        temp = Math.max(first, second);
+        result = Math.max(temp, third);
+        System.out.println(result);
     }
 }
